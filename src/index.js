7fb8b4524e5fc9cee4f34e577e2975d9dd834249
imkeshav98 <imkeshav98@gmail.com>
2022-06-05 14:25:50 +0530

    cors added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const { login, register } = require("./controllers/auth.controller");
 const app = express();
+const cors = require("cors");
 
 app.use(express.json()); // parse json
+app.use(cors());
 
 app.use("/api/artists", require("./controllers/artist.controller")); // artist routes
 app.use("/api/songs", require("./controllers/song.controller")); // song routes
